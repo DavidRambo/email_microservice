0f05a6c1ecc6c0024eaf6aa1a08f3c90af88592c
David Rambo <davidrambo@mailfence.com>
2024-11-30 13:54:23 -0800

    validator classes for emails

--- email-microservice/main.py
+++ email-microservice/main.py
@@ -1,8 +1,11 @@
 """FastAPI application for sending emails."""
 
 import fastapi
+import pydantic
 
 from fastapi.middleware.cors import CORSMiddleware
+
+from config import settings
 
 app = fastapi.FastAPI()
 
@@ -19,3 +22,21 @@ app.add_middleware(
     allow_methods=["GET", "POST", "PATCH", "DELETE"],
     allow_headers=["*"],
 )
+
+
+class EmailBase(pydantic.BaseModel):
+    """Required fields for creating an email."""
+
+    recipient_name: str
+    recipient_addr: pydantic.EmailStr
+    subject: str = pydantic.Field(default="A Message from the Wish Lists App")
+    message: str
+
+
+class EmailSend(EmailBase):
+    """Required fields for sending an email."""
+
+    mail_server: str = pydantic.Field(default=settings.mail_server)
+    mail_port: int = pydantic.Field(default=settings.mail_port)
+    sender: pydantic.EmailStr = pydantic.Field(default=settings.sender_addr)
+    password: str = pydantic.Field(default=settings.sender_password)
